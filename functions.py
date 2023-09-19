5a6fa0f47ae20e5a4d7c0fb02fcd197ffa741503
cristian-nambo <cristian-nambo@hotmail.com>
2023-09-18 17:28:28 -0700

    File and intructions created

---
+++ functions.py
@@ -0,0 +1,4 @@
+# Write a Python function called max_num()to find the maximum of three numbers.
+# Write a Python function called mult_list() to multiply all the numbers in a list.
+# Write a Python function called rev_string() to reverse a string.
+# Write a Python function called num_within() to check whether a number falls in a given range.
